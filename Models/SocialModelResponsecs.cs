472134b14ef26d5ac98975efb8ea509f50d04e8e
IvanHalych <39216494+IvanHalych@users.noreply.github.com>
2021-03-22 23:10:15 +0200

    fix admin api

---
+++ Models/SocialModelResponsecs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PMAuth.Models
+{
+    public class SocialModelResponsecs
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public SocialModelResponsecs(int id, string name)
+        {
+            Id = id;
+            this.name = name;
+        }
+    }
+}
