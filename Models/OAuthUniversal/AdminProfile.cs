7ba91bfe6fb4a3f139309e19293ef8135e2d03ce
IvanHalych <39216494+IvanHalych@users.noreply.github.com>
2021-03-25 21:56:10 +0200

    some fix admin controler

--- Models/OAuthUniversal/AdminProfile.cs
+++ Models/OAuthUniversal/AdminProfile.cs
@@ -14,5 +14,17 @@ namespace PMAuth.Models.OAuthUniversal
         /// Access token what need from authentication
         /// </summary>
         public string Token { get; set; }
+
+        public AdminProfile(UserProfile user)
+        {
+            Id = user?.Id;
+            FirstName = user?.FirstName;
+            LastName = user?.LastName;
+            Email = user?.Email;
+            IsVerifiedEmail = user?.IsVerifiedEmail;
+            Photo = user?.Photo;
+            Locale = user?.Locale;
+            AdditionalInformation = user?.AdditionalInformation;
+        }
     }
 }
