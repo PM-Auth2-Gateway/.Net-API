f3427d6a4d39e786752cc430b295beea63f3b162
Dennis Sakadel <denyssakadel@gmail.com>
2021-03-26 22:18:50 +0200

    added RefreshTokens migration and model

--- AuthDbContext/BackOfficeContext.cs
+++ AuthDbContext/BackOfficeContext.cs
@@ -4,26 +4,31 @@ using PMAuth.AuthDbContext.Entities;
 namespace PMAuth.AuthDbContext
 {
     /// <summary>
-    /// BackOfficeContext
+    /// Data Base Context that operate tables in ORM system
     /// </summary>
     public class BackOfficeContext : DbContext
     {
         /// <summary>
-        /// Admins DbSet
+        /// Data Base table - Admins
         /// </summary>
         public DbSet<Admin> Admins { get; set; }
         /// <summary>
-        /// Apps DbSet
+        /// Data Base table - Apps
         /// </summary>
         public DbSet<App> Apps { get; set; }
         /// <summary>
-        /// Socials DbSet
+        /// Data Base table - Socials
         /// </summary>
         public DbSet<Social> Socials { get; set; }
         /// <summary>
-        /// Settings DbSet
+        /// Data Base table - Settings
         /// </summary>
         public DbSet<Setting> Settings { get; set; }
+        /// <summary>
+        /// Data Base table - RefreshTokens
+        /// </summary>
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
         /// <summary>
         /// BackOfficeContext constructor
         /// </summary>
---
+++ AuthDbContext/Entities/RefreshToken.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PMAuth.AuthDbContext.Entities
+{
+    /// <summary>
+    /// Entity that handle refresh token and his expires time.
+    /// </summary>
+    public class RefreshToken
+    {
+        /// <summary>
+        /// Refresh Token Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Handle Refresh Token Value
+        /// </summary>
+        public string RefreshTokenValue { get; set; }
+
+        /// <summary>
+        /// Expires time of refresh token.
+        /// </summary>
+        public DateTime ExpiresTime { get; set; }
+
+    }
+}
