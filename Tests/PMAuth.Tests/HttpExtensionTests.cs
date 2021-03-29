f5336f002b0b3537e431492306b0a2757d6c63e8
Vadym Mulish <kabanborov43346@gmail.com>
2021-03-29 03:14:35 +0300

    Added tests for HttpExtension

---
+++ Tests/PMAuth.Tests/HttpExtensionTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using PMAuth.Extensions;
+using Xunit;
+
+namespace PMAuth.Tests
+{
+    public class HttpExtensionTests
+    {
+        [Fact]
+        public void AddQuery_SingleQuery_ExpectedQueryMatchesActual()
+        {
+            //arrange
+            string expectedUri = "https://google.com/?name=value";
+            Uri baseUri = new Uri("https://google.com");
+            
+            //act
+            string actualUri = baseUri.AddQuery("name", "value").ToString();
+
+            //assert
+            actualUri.Should().Be(expectedUri);
+        }
+        
+        [Fact]
+        public void AddQuery_DuplicateQuery_QueryValueMatchesLastAddedValue()
+        {
+            //arrange
+            string expectedUri = "https://google.com/?name=value2";
+            Uri baseUri = new Uri("https://google.com");
+            
+            //act
+            string actualUri = baseUri.AddQuery("name", "value1").ToString();
+            actualUri = baseUri.AddQuery("name", "value2").ToString();
+
+            //assert
+            actualUri.Should().Be(expectedUri);
+        }
+        
+        [Fact]
+        public void AddQuery_NullQueryParamName_QueryMatchesExpected()
+        {
+            //arrange
+            string expectedUri = "https://google.com/";
+            Uri baseUri = new Uri("https://google.com");
+            
+            //act
+            string actualUri = baseUri.AddQuery(null, "value").ToString();
+            
+            //assert
+            actualUri.Should().Be(expectedUri);
+        }
+        
+        [Fact]
+        public void AddQuery_NullQueryParamValue_QueryMatchesExpected()
+        {
+            //arrange
+            string expectedUri = "https://google.com/";
+            Uri baseUri = new Uri("https://google.com");
+            
+            //act
+            string actualUri = baseUri.AddQuery("name", null).ToString();
+            
+            //assert
+            actualUri.Should().Be(expectedUri);
+        }
+    }
+}
